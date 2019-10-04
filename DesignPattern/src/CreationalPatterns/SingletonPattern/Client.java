0dcc8c9885fe96aeccaa65cf0ed202a32cf749f4
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-10-04 19:24:56 +0600

    singleton pattern with lazy creation mechanism is implemented

---
+++ DesignPattern/src/CreationalPatterns/SingletonPattern/Client.java
@@ -0,0 +1,9 @@
+package CreationalPatterns.SingletonPattern;
+
+public class Client
+{
+    public static void main(String args[])
+    {
+
+    }
+}
---
+++ DesignPattern/src/CreationalPatterns/SingletonPattern/LazySingleton.java
@@ -0,0 +1,21 @@
+package CreationalPatterns.SingletonPattern;
+
+public class LazySingleton
+{
+    private static LazySingleton instance;
+
+    private LazySingleton()
+    {
+
+    }
+
+    public static LazySingleton getInstance()
+    {
+        if(instance==null)
+        {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+
+}
